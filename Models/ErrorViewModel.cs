8b52016164c94a949586070d8abcb10a83481cfb
parmenides <aysenur.yildirim14@ogr.sakarya.edu.tr>
2023-12-13 20:16:18 +0300

    Proje dosyası ekle.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HastaneOtomasyonSistemi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
